1c11dddc6efa3c59e0fe31e45ccc724cd5a48517
李兴东 <lixingdong@lixingdong.local>
2016-05-16 22:31:17 +0800

    add a new project

---
+++ music/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  music
+//
+//  Created by 李兴东 on 16/5/16.
+//  Copyright © 2016年 leek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
